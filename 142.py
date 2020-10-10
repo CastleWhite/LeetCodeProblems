472f55c8b6f5c70eae69bd27e2fbab5de8bf1a6b
CastleWhite <49804857+CastleWhite@users.noreply.github.com>
2020-10-10 18:26:04 +0800

    Create 142.py

---
+++ 142.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        fast = head
+        slow = head
+        while fast:
+            fast = fast.next
+            if not fast:
+                return None
+            fast = fast.next
+            slow = slow.next
+            if fast == slow:
+                a = head
+                while a != slow:
+                    a = a.next
+                    slow = slow.next
+                return a
