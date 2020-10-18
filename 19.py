ec92b1c4793e51e7968987a9541c4039b2d657c1
CastleWhite <49804857+CastleWhite@users.noreply.github.com>
2020-10-18 12:16:22 +0800

    Update 19.py

--- 19.py
+++ 19.py
@@ -15,5 +15,6 @@ class Solution:
             fast = fast.next
             slow = slow.next
         slow.next = slow.next.next
-
+## 如果我们能够得到的是倒数第 n个节点的前驱节点而不是倒数第 n个节点的话，删除操作会更加方便。
+## 添加哑结点，在头结点前
         return head
