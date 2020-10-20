db08f5a234bb7edfc743142acde157beeb149c04
CastleWhite <49804857+CastleWhite@users.noreply.github.com>
2020-10-20 12:24:44 +0800

    Create 143.py

---
+++ 143.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        l = []
+        a = head
+        while a:
+            l.append(a)
+            a = a.next
+
+        a = head
+        for i in range((len(l)-1) // 2):
+            l[-1-i].next = a.next
+            a.next = l[-1-i]
+            a = a.next.next
+        l[-1-((len(l)-1) // 2)].next = None
