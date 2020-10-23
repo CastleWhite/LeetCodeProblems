486915fa114d4692444cdd03980b5c2542cc0693
CastleWhite <49804857+CastleWhite@users.noreply.github.com>
2020-10-23 19:11:24 +0800

    Create 234.py

---
+++ 234.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return True
+            
+        fast = head
+        slow = head
+        reco = []
+        while fast.next:
+            reco.append(slow.val)
+            fast = fast.next
+            if fast.next:
+                fast = fast.next
+                slow = slow.next
+        
+        while slow.next:
+            slow = slow.next
+            if slow.val != reco.pop():
+                return False
+
+        return True
+            
+                
