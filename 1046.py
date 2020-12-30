ac535218be19af7c69016ae25d8f2e5c0fb21e52
CastleWhite <49804857+CastleWhite@users.noreply.github.com>
2020-12-30 09:00:33 +0800

    Update 1046.py

--- 1046.py
+++ 1046.py
@@ -1,17 +1,11 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        for i in range(len(stones)):
-            stones[i] = -stones[i]
-
+        stones = [-i for i in stones]
         heapify(stones)
         while len(stones)>1:
-            a = heappop(stones)
-            b = heappop(stones)
-            if a<b:
-                heappush(stones,a-b)
-            
-
-        if stones:
-            return -stones[0]
-        else:
-            return 0
+            x = heappop(stones)
+            y = heappop(stones)
+            remain = x-y
+            if remain!=0:
+                heappush(stones,remain)
+        return -stones[0] if stones else 0
